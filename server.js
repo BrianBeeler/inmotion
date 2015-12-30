7b45d5afc27bee7a217113bd86be8aea78746d5d
Brian Beeler <brianlbeeler@gmail.com>
2015-12-30 10:51:26 -0600

    Set up server to run app

---
+++ server.js
@@ -0,0 +1,13 @@
+// Server used to run app
+
+var express = require('express');
+var app = express();
+
+
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname));
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+})
