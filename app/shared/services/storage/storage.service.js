2ba9b98449a3c66c93f6e868aedaff19f966a129
Brian Beeler <brianlbeeler@gmail.com>
2015-12-31 10:17:12 -0600

    Added some dummydata movies

--- app/shared/services/storage/storage.service.js
+++ app/shared/services/storage/storage.service.js
@@ -2,10 +2,41 @@ angular.module("movieApp")
 
   .factory("Storage", function() {
 
-    var movies = [];
+    // Give site some initial movie data
+    var movies = [
+      { title: "Star Wars: The Force Awakens",
+        genre: "Fantasy/Science fiction",
+        actors: "Daisy Ridley, John Boyega, Harrison Ford",
+        year: 2015,
+        rating: 8.6
+       },
+       { title: "Daddy's Home",
+        genre: "Comedy",
+        actors: "Will Ferrell, Linda Cardellini, Mark Wahlberg",
+        year: 2015,
+        rating: 6.6
+       },
+       { title: "Joy" ,
+        genre: "Drama film/History",
+        actors: "Jennifer Lawrence" ,
+        year: 2015,
+        rating: 6.6
+       },
+       { title: "Titanic" ,
+        genre: "Drama, Romance",
+        actors: "Leanardo DiCaprio, Kate Winslet",
+        year: 1997,
+        rating: 7.7
+       }
+    ];
 
+    // Storage service is used to add movies to list, or get all the movies
     var Storage = {}
+
+    // Get all movies
     Storage.get = function() { return movies }
+
+    // Add a movie to the list
     Storage.add = function(movie) {
       movies.push({
         title : movie.title,
@@ -15,6 +46,6 @@ angular.module("movieApp")
         rating: movie.rating
       })
     }
-    
+
     return Storage
   })
